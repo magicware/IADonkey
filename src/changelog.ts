eac36b3522107e068e5b07989c24088c272d4d03
coolhanek <petr.kulhanek@magicware.cz>
2026-09-16 22:43:03 +0200

    chore: Release v1.1.9

--- src/changelog.ts
+++ src/changelog.ts
@@ -5,7 +5,7 @@ export interface VersionEntry {
   highlights: string[];
 }
 
-export const CURRENT_APP_VERSION = '1.1.8';
+export const CURRENT_APP_VERSION = '1.1.9';
 
 /**
  * Uživatelsky přívětivá historie verzí (ne technický žargon, ale přehled reálných funkcí pro uživatele).
